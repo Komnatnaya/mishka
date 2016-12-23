962f9a1e23e28e779c0ea429fc13d45847274d3b
Katerina Komnatnaya <comina.ru@gmail.com>
2016-12-23 21:43:01 +0200

    fix+

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//Открытие меню в мобильно версии
+
 var navMain = document.querySelector('.main-nav');
 var navButton = document.querySelector('.page-header__toggle-nav');
 var navTorggle = document.querySelector('.page-header__toggle');
@@ -21,3 +23,26 @@ navButton.addEventListener('click', function(event) {
     navTorggle.classList.add('page-header__toggle--close-nav');
   }
 });
+
+//Открытие модального окна
+
+var link = document.querySelectorAll('.modal-btn');
+
+if(link.length > 0) {
+  var modal = document.querySelector('.modal');
+  var overlay = document.querySelector('.overlay');
+
+  for(var i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      modal.classList.add('modal--show');
+      overlay.classList.add('overlay--show');
+    })
+  }
+
+  overlay.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.classList.remove('modal--show');
+    overlay.classList.remove('overlay--show');
+  })
+}
