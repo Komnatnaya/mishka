ed8eaf32f8421abb10ede217e7c793d8c453ec79
Katerina Komnatnaya <comina.ru@gmail.com>
2016-12-24 01:42:45 +0200

    build

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Маркет на карте на главной странице.
+//Маркер на карте на главной странице.
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
